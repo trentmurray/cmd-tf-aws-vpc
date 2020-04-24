9aa3f6b0fb43d570bcdd5b9d4d6a37cd01e99157
Dean Illfeld <dean.illfeld@cmdsolutions.com.au>
2020-04-24 17:09:25 +1000

    move region to envvvar

--- tests/backend.tf
+++ tests/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket                  = "cmdlabtf-terraform-backend"
     key                     = "module-cmd-tf-aws-vpc"
-    region                  = "ap-southeast-2"
     profile                 = "cmdlabtf-tfbackend"
     dynamodb_table          = "cmdlabtf-terraform-lock"
     skip_metadata_api_check = true
--- tests/provider.tf
+++ tests/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   version                 = "2.53"
-  region                  = "ap-southeast-2"
   profile                 = "cmdlabtf-master"
   skip_metadata_api_check = true
 }
